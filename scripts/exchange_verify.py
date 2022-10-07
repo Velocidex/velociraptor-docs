b0b15aa9e02981724476dabe08c41e5bf29572e2
Wes <wlambertts@gmail.com>
2022-10-07 13:13:55 +0000

    Verify artifact file extension is correct

--- scripts/exchange_verify.py
+++ scripts/exchange_verify.py
@@ -30,9 +30,13 @@ EXCHANGE_PATH = os.path.abspath("./content/exchange/artifacts/")
 VELO_LOGFILE = "/tmp/velo.log"
 
 # Verify artifact contains correct file extension
+# Skip other specific file types
+VALID_EXTENSIONS = (".yaml", ".gitignore", ".jpg", ".png")
+
 for f in os.listdir(EXCHANGE_PATH):
-    if not f.endswith('.yaml'):
-        raise Exception("ERROR: File {0} does not contain a valid extension!".format(f))
+    if os.path.isfile(f):
+        if not f.endswith(VALID_EXTENSIONS):
+            raise Exception("ERROR: File {0} does not contain a valid extension!".format(f))
 
 with open(VELO_CONFIG_FILENAME, "w") as fd:
     fd.write(VELO_CONFIG)
