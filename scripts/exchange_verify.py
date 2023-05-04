fef934c36ffbf85a2345dd431aa74e9324504eb8
Mike Cohen <mike@velocidex.com>
2023-05-04 13:06:51 +1000

    Update binary used to validate artifacts.
    New binary supports newer options.

--- scripts/exchange_verify.py
+++ scripts/exchange_verify.py
@@ -25,7 +25,8 @@ Datastore:
 '''
 VELO_CONFIG_FILENAME = "/tmp/velo.config.yaml"
 VELO_FILENAME = "/tmp/velociraptor"
-VELO_URL = "https://github.com/Velocidex/velociraptor/releases/download/v0.6.4-2/velociraptor-v0.6.4-2-linux-amd64"
+#VELO_URL = "https://github.com/Velocidex/velociraptor/releases/download/v0.6.4-2/velociraptor-v0.6.4-2-linux-amd64"
+VELO_URL="https://storage.googleapis.com/go.velocidex.com/velociraptor-v0.6.9-dev-linux-amd64-musl"
 EXCHANGE_PATH = os.path.abspath("./content/exchange/artifacts/")
 VELO_LOGFILE = "/tmp/velo.log"
 
